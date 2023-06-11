791618a523283d15b43c6d6782cbc17f57a95636
saaid <saaid2538@gmail.com>
2023-06-11 02:20:10 -0700

    added 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
     b = []
+    c = None
     for i in my_string:
         if i != "c" and i != "C":
             b.append(i)
-    return "".join(b)
+        c = "".join(b)
+    return c
