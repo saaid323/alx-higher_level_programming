f8442a8e3e281493c40ee8c5c850691c098ce01b
saaid <saaid2538@gmail.com>
2023-06-12 22:44:57 -0700

    added 100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,18 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p) {
+    Py_ssize_t size = PyList_Size(p);
+
+    printf("[*] Size of the Python List = %ld\n", size);
+
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (Py_ssize_t i = 0; i < size; i++) {
+        PyObject *item = PyList_GetItem(p, i);
+        const char *typeName = Py_TYPE(item)->tp_name;
+
+        printf("Element %ld: %s\n", i, typeName);
+    }
+}
