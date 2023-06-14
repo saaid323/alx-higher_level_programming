1b90d18bd326c96c08aaf0109818f977df885cc3
saaid <saaid2538@gmail.com>
2023-06-14 08:18:32 -0700

    added 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None and len(a_dictionary) != 0:
+    if a_dictionary:
+        new = list(a_dictionary.keys())
+        largest = 0
+        name = ''
+        for i in new:
+            if a_dictionary[i] > largest:
+                largest = a_dictionary[i]
+                name = i
         largest = max(list(a_dictionary.keys()))
 
-        return largest
-    return None
+        return name
