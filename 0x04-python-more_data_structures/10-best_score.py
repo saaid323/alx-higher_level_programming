513cfdaececbdb92f104d9dc90a4a32c8d3200a8
saaid <saaid2538@gmail.com>
2023-06-14 03:32:51 -0700

    added 11-multiply_list_map.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) != 0:
         largest = max(list(a_dictionary.keys()))
 
         return largest
