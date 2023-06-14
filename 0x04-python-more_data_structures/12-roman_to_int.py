d128b189ddc0d8cee445cc5939472bf28fd08f68
saaid <saaid2538@gmail.com>
2023-06-14 12:21:28 -0700

    added 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,14 @@
 def roman_to_int(roman_string):
     r_dict = {"X": 10, "V": 5, "I": 1, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
+    pre = 0
     if isinstance(roman_string, str):
-        for i in roman_string:
-            if i in r_dict:
-                if roman_string[0] == 'I' and roman_string[1] != 'I':
-                    total -= 1
-                total += r_dict[i]
+        for i in roman_string[::-1]:
+            value = r_dict[i]
+            if value < pre:
+                total -= value
+            else:
+                total += value
+            pre = value
         return total
     return 0
