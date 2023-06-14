22c6f051f56b7a70f1fb47ae0a926358e1ab62f0
saaid <saaid2538@gmail.com>
2023-06-14 03:40:52 -0700

    added 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total = 0
+    total_1 = 0
+    if len(my_list) == 0:
+        for i in my_list:
+            total += i[0] * i[1]
+            total_1 += i[1]
+        return total / total_1
+    return 0
