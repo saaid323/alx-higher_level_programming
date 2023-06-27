7ea0a21c9457c94227e050284b313bf451f23d30
saaid <saaid2538@gmail.com>
2023-06-27 03:51:32 -0700

    added 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+
+"""A Square"""
+
+
 class Square:
-    pass
+    """Size Of Square"""
+    def __init__(self, size):
+        """
+        Initialize  size of square
+        """
+        self.__size = size
