68bb3296e6bbf4e70d36b07aa0b670264ef582da
saaid <saaid2538@gmail.com>
2023-06-27 13:02:59 -0700

    added 100-singly_linked_list.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""A Node"""
+
+
+class Node:
+    """Node"""
+    def __init__(self, data, next_node=None):
+        """""Initiate size of Square"""""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+"""Link List"""
+
+
+class SinglyLinkedList:
+    """SinglyLinkedList"""
+    def __init__(self):
+        """Initiate SinglyLinkedList"""
+        self.head = None
+
+    def __str__(self):
+        if self.head is None:
+            return ""
+        current = self.head
+        nodes = []
+        while current:
+            nodes.append(str(current.data))
+            current = current.next_node
+        return "\n".join(nodes)
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        elif value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
