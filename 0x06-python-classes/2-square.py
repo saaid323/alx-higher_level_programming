62d5152fcda0b403a4221fa193d12f8a8fe1c2d1
saaid <saaid2538@gmail.com>
2023-06-27 04:11:02 -0700

    added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A Square"""
+
+
+class Square:
+    """Square"""
+    def __init__(self, size=0):
+        """Initiating size of Square"""
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
