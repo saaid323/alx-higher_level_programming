1e0535f4e7bce0ef789ecef1a34a7657bb228280
saaid <saaid2538@gmail.com>
2023-07-03 03:07:11 -0700

    added 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
-            return ""
+            return ("")
         return "\n".join(["#" * self.__width for i in range(self.height)])
 
     def __repr__(self):
