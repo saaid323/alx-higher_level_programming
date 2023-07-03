81a5ea95fb4fdb003bf52933a9e3f0b67459ab0d
saaid <saaid2538@gmail.com>
2023-07-03 04:44:33 -0700

    added 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,10 +10,11 @@ class Rectangle:
     """
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
-        Rectangle.number_of_instances += 1
         self.height = height
         self.width = width
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
