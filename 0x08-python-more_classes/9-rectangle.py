68d533e18e61cf889d0d6d9356cdbd941ee66712
saaid <saaid2538@gmail.com>
2023-07-03 14:08:45 -0700

    added 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -49,6 +49,10 @@ class Rectangle:
             return 0
         return (self.__height + self.__width) * 2
 
+    @classmethod
+    def square(cls, size=0):
+        return (cls(size, size))
+
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
@@ -72,7 +76,3 @@ class Rectangle:
             return rect_1
         elif rect_2.area() > rect_1.area():
             return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        return cls(size, size)
