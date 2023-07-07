5f897eb62f4d6feef801775c87ca7b0c3566d0b7
saaid <saaid2538@gmail.com>
2023-07-07 09:34:09 -0700

    added 5-text_indentation.py, tests/5-text_indentation.txt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+prints a text with 2 new lines after each of these characters: ., ? and :
+"""
 
 
 def text_indentation(text):
+    """
+     function that prints a text with 2 new lines after
+     each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     flag = 0
