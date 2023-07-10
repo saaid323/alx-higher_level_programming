2c5594b64e71a578d6a2937f0f0a1d7fdee53cc6
saaid <saaid2538@gmail.com>
2023-07-10 13:54:28 -0700

    added 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,6 +6,9 @@ if it’s possible
 
 
 def add_attribute(obj, name, value):
+    """
+    adds a new attribute to an object if it’s possible
+    """
     if isinstance(obj, str):
         raise TypeError("can't add new attribute")
     obj.__setattr__(name, value)
