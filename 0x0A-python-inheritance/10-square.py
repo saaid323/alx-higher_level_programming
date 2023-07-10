3afe5745cf1a145732e663518aba192d5b6e23e2
saaid <saaid2538@gmail.com>
2023-07-10 08:03:24 -0700

    added 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,10 +12,5 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-        return self.__size * self.__size
-
-    def __str__(self):
-        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
