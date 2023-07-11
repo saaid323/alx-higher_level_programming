179795e37930975148f02257aca2ae8cb39d2db2
saaid <saaid2538@gmail.com>
2023-07-11 02:00:41 -0700

    added 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,4 @@ def read_file(filename=""):
     function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, mode="r") as f:
-        f = f.readlines()
-    for i in f:
-        print(i)
+        print(f.read())
