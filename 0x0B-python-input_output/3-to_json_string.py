cf03f6f1918d139253f7bfb187867b2aed9be272
saaid <saaid2538@gmail.com>
2023-07-11 03:42:46 -0700

    added 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+"""
+returns the JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+     function that returns the JSON representation of
+     an object (string)
+    """
+    return json.dumps(my_obj)
