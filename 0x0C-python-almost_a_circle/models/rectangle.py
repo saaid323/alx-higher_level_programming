9ba2b0fd86acf068a4801f651f094231f69a40f4
saaid <saaid2538@gmail.com>
2023-07-14 21:32:33 -0700

    added models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,7 +67,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-        def area(self):
+    def area(self):
         return self.__height * self.__width
 
     def display(self):
@@ -76,3 +76,26 @@ class Rectangle(Base):
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args, **kwargs):
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
+        for key, value in kwargs.items():
+            if key == 'id':
+                self.id = value
+            elif key == 'width':
+                self.__width = value
+            elif key == 'height':
+                self.__height = value
+            if key == 'x':
+                self.__x = value
+            if key == 'y':
+                self.__y = value
