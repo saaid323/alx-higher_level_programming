e0e3887cf3dc3f1cd771117f912801b624b833be
saaid <saaid2538@gmail.com>
2023-07-17 13:11:01 -0700

    edited base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -81,17 +81,15 @@ class Base:
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """ writes list_objs to a csv file"""
-        file_1 = cls.__name__ + '.csv'
-        fieldnames = []
-        if cls.__name__ == 'Rectangle':
-            fieldnames = ['id', 'width', 'height', 'x', 'y']
-        if cls.__name__ == 'Square':
-            fieldnames = ['id', 'size', 'x', 'y']
-        with open(file_1, mode='w') as f:
-            csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
-            csv_writer.writeheader()
-            for i in list_objs:
-                csv_writer.writerow(i.to_dictionary())
+        filename = cls.__name__ + ".csv"
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            if cls.__name__ == "Rectangle":
+                for obj in list_objs:
+                    writer.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+            elif cls.__name__ == "Square":
+                for obj in list_objs:
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
 
     @classmethod
     def load_from_file_csv(cls):
