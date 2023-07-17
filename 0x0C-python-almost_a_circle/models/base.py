78372509862271e5ab5ca83ae3c7a7d87b90839a
saaid <saaid2538@gmail.com>
2023-07-17 12:51:30 -0700

    edited base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
         """returns the JSON string representation
         of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
-            return '[]'
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
