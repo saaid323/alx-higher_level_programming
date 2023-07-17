b4e0e5d5ad41c05d3b6b36722f6e7f0c7b3f8344
saaid <saaid2538@gmail.com>
2023-07-17 12:29:31 -0700

    edited models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,22 +9,29 @@ class Square(Rectangle):
     """
     Define square
     """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializing square"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """Property of square"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """setter of size"""
         self.width = value
         self.height = value
 
     def __str__(self):
+        """return [Square] (<id>) <x>/<y> - <size>"""
         return f"[square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     def update(self, *args, **kwargs):
+        """print in stdout the Rectangle instance
+        with the character # by taking care of x and y"""
         if len(args) >= 1:
             self.id = args[0]
         if len(args) >= 2:
@@ -44,4 +51,5 @@ class Square(Rectangle):
                 self.y = value
 
     def to_dictionary(self):
+        """assigns an argument to each attribute"""
         return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
