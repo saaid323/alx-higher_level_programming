a6fdc87af7dacbb54bcf0dc5f19a8008709e6445
saaid <saaid2538@gmail.com>
2023-08-15 14:37:42 -0700

    added 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_tabl
-SELECT score AND name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
