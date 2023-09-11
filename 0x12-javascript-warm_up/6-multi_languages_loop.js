39b0250262ce173ce447649eddf0a10f6f3ec44e
saaid <saaid2538@gmail.com>
2023-09-11 02:14:38 -0700

    added 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+for (let i = 0; i < 3; i++) {
+  console.log(['C is fun', 'Python is cool', 'JavaScript is amazing'][i]);
 }
