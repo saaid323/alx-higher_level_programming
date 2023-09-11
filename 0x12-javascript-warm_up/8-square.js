f4b545dfe90227d92db4db4a295aad62b8607829
saaid <saaid2538@gmail.com>
2023-09-11 10:25:54 -0700

    added 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-const x = parseInt(process.argv[2]);
 if (argv[2] === undefined || isNaN(argv[2])) {
   console.log('Missing size');
 } else {
+  const x = parseInt(process.argv[2]);
   for (let i = 0; i < x; i++) {
     console.log('X'.repeat(x));
   }
