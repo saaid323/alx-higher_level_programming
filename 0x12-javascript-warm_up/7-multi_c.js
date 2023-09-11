68ef07ff3ed5de926c5cafb9d3f2f2fa5e8fe6c8
saaid <saaid2538@gmail.com>
2023-09-11 05:59:53 -0700

    added 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,10 @@ import { argv } from 'node:process';
 
 let i = 0;
 if (argv[2] === undefined || isNaN(argv[2])) {
-	console.log('Missing number of occurrences');
-}
-while (i < argv[2]) {
-  console.log('C is fun');
-  i++;
+  console.log('Missing number of occurrences');
+} else {
+  while (i < argv[2]) {
+    console.log('C is fun');
+    i++;
+  }
 }
