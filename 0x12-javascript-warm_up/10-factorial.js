6bc6fe91abc68d02177da406e2554badcf5e18af
saaid <saaid2538@gmail.com>
2023-09-11 09:39:00 -0700

    added 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 
 function factorial (n) {
   if (isNaN(n)) {
-    return 1; // Factorial of NaN is 1
+    return 1;
   } else if (n <= 1) {
-    return 1; // Factorial of 0 and 1 is 1
+    return 1;
   } else {
-    return n * factorial(n - 1); // Recursive call to compute factorial
+    return n * factorial(n - 1);
   }
 }
 
