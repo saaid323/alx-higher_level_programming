7e5595f7096d1c253f7c1da0bfeff503f9a5d9fd
saaid <saaid2538@gmail.com>
2023-09-11 05:29:32 -0700

    added 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-if (argv[2] === undefined) {
+if (argv.length === 0) {
   console.log('No argument');
-} else {
+} else if (args.length === 1) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
