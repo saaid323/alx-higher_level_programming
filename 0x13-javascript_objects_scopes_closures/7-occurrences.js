c7848d9cef437407f8ea36cdcf088d594907b29c
saaid <saaid2538@gmail.com>
2023-09-12 05:31:31 -0700

    added 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  occ = 0;
+  let occ = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] == searchElement) {
+    if (list[i] === searchElement) {
       occ += 1;
     }
   }
   return occ;
-}
+};
