7d01c4247a565602ee09a4a8d5ce55067ef9acf7
saaid <saaid2538@gmail.com>
2023-09-13 01:02:26 -0700

    added 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
-if (argv.length <= 3) {
+
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arr = argv.slice(2).map(Number);
+  const arr = process.argv.slice(2).map(Number);
   const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
