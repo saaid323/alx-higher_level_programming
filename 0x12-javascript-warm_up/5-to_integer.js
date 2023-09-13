371ea287f240b1dca1c2f8c69bbbea36b36d1ff0
saaid <saaid2538@gmail.com>
2023-09-13 00:59:32 -0700

    added 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-import { argv } from 'node:process';
 
-const num = (parseInt(argv[2]));
+const num = (parseInt(process.argv[2]));
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
