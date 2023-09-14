090946d945c77519d2eca34aee6a613c14940fc5
saaid <saaid2538@gmail.com>
2023-09-14 05:34:30 -0700

    added 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
             password=sys.argv[2],
             database=sys.argv[3], host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name={sys.argv[4]} ORDER BY id ASC")
+    query = ("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC")
+    cur.execute(query, (sys.argv[4] + '%',))
     rows = cur.fetchall()
     for row in rows:
         print(row)
