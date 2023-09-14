63a08a3bd5a70b050ad5de21cf49539e5f979a1a
saaid <saaid2538@gmail.com>
2023-09-14 05:19:38 -0700

    added 1-filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+"""script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 import sys
 
@@ -10,7 +10,7 @@ if __name__ == '__main__':
             password=sys.argv[2],
             database=sys.argv[3], host='localhost', port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(f"SELECT * FROM states WHERE name={sys.argv[4]} ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
