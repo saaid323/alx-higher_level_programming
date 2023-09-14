89240781f7c491c5b45abe2bf19fbc5a98720daa
saaid <saaid2538@gmail.com>
2023-09-14 04:11:34 -0700

    added 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], host='localhost', port=3306)
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
