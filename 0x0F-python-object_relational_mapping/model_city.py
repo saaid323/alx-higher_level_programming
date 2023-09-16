7bc7ea9c6d1d702b608a70d99aaa6176b38223f5
saaid <saaid2538@gmail.com>
2023-09-16 05:07:59 -0700

    added model_city.py, 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from model_state import Base
 
 
 class City(Base):
+    """ contains the class definition of a City"""
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
