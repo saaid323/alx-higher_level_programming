a7d2c3a182c8869d94b72e9eb7e2cb86f5826720
saaid323 <saaid2538@gmail.com>
2023-09-28 05:39:09 -0700

    added 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-length=$(echo "$(curl -sI $1)" | grep -i  '^content-length:' | awk '{print $2}')
-echo $length
+echo "$(curl -sI $1)" | grep -i  '^content-length:' | awk '{print $2}'
