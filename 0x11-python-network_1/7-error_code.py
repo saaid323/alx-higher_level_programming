ae9a76cf02de5affce91ba857476f697d1dc9bf1
saaid323 <saaid2538@gmail.com>
2023-09-29 12:41:21 -0700

    added 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,8 +5,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.text)
-    except requests.exceptions.RequestException as e:
-        print("Error code: ", e.status_code)
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print("ndjfn", r.text)
