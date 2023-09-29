3e3ad85c25cb8f5b522aa19c293396171f6531d4
saaid323 <saaid2538@gmail.com>
2023-09-29 14:20:41 -0700

    added 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that takes in a letter and sends a POST request to 
+"""script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter."""
 import requests
 import sys
@@ -20,4 +20,3 @@ if __name__ == "__main__":
             print("No result")
     except ValueError:
         print("Not a valiid JSON")
-
