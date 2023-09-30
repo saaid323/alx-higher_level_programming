ccac4ed3bf89be6cf35245d4e76edf9588e31e5e
saaid323 <saaid2538@gmail.com>
2023-09-30 04:58:06 -0700

    added 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" script that takes your GitHub credentials (username and password) and
-uses the GitHub API to display your id"""
+"""script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    pat = sys.argv[2]
-    url = f"https://api.github.com/user"
-    auth = (username, pat)
-    response = requests.get(url, auth=auth)
-    if response.status_code == 200:
-        user_info = response.json()
-        user_id = user_info['id']
-        print(f"Your GitHub user ID is: {user_id}")
-    else:
-        print("None")
+    user = sys.argv[1]
+    password = sys.argv[2]
+    auth=(user, password)
+    r = requests.get('https://api.github.com/user', auth=auth)
+    user = r.json()
+    try:
+        user_id = user['id']
+        print(user['id'])
+    except KeyError:
+        print('None')
