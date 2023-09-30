bf78794c68ca04f8ab35f22fe01e644af98a8b8c
saaid323 <saaid2538@gmail.com>
2023-09-30 04:26:06 -0700

    added 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     q = ''
     if len(sys.argv) > 1:
         q = sys.argv[1]
-    data = {"q": q}
+    data = {'q': q}
     r = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
         result = r.json()
@@ -16,5 +16,6 @@ if __name__ == "__main__":
             print(f"[{result['id']}] {result['name']}")
         else:
             print("No result")
+
     except ValueError:
-        print("Not a valiid JSON")
+        print("Not a valid JSON")
