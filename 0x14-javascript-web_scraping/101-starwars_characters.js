0c3f14dc740db4d9a38f43e47fcd7bbb768b717b
saaid323 <saaid2538@gmail.com>
2023-10-24 11:27:06 -0700

    added 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,13 +5,21 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, res, body) => {
   if (!err && res.statusCode === 200) {
     const films = JSON.parse(body);
-    for (const person of films.characters) {
-      request(person, (err, res, body) => {
-        if (!err && res.statusCode === 200) {
-          const pers = JSON.parse(body);
-          console.log(pers.name);
-        }
-      });
-    }
+    const character = films.characters;
+    fetchCharacterNames(character);
   }
 });
+
+function fetchCharacterNames (characterUrls, index = 0) {
+  if (index >= characterUrls.length) {
+    return;
+  }
+
+  request.get(characterUrls[index], (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const character = JSON.parse(body);
+      console.log(character.name);
+      fetchCharacterNames(characterUrls, index + 1);
+    }
+  });
+}
