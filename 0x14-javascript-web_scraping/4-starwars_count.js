a70a804fb62a57a1d98571d290e238962fde2e47
saaid323 <saaid2538@gmail.com>
2023-10-24 06:37:59 -0700

    added 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    let total = body.split('/people/18/').length - 1;
+    const total = body.split('/people/18/').length - 1;
 
     console.log(total);
   }
