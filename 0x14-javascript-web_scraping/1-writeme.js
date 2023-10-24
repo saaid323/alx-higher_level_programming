d7c5db2ff4c218c030c5c06fd6b37b0bf5dbee00
saaid323 <saaid2538@gmail.com>
2023-10-24 04:16:44 -0700

    added 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-path = process.argv[2];
-content = process.argv[3];
+const path = process.argv[2];
+const content = process.argv[3];
 fs.writeFile(path, content, (err) => {
   if (err) {
     console.log(err);
