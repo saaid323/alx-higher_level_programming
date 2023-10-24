e212219bb9b0424c0963a45cfabf687ae3774882
saaid323 <saaid2538@gmail.com>
2023-10-24 04:29:07 -0700

    added 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const url = process.argv[2];
 request(url, (eror, response, body) => {
-  console.log(response.statusCode);
+  console.log("code: ", response.statusCode);
 });
