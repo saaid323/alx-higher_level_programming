9f0d6e3c2220c467d667bdade7bca1c8d988057a
saaid323 <saaid2538@gmail.com>
2023-10-24 06:08:48 -0700

    added 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request.get(url, (error, response, body) => {
+request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const film = JSON.parse(body);
     const id = 'https://swapi-api.alx-tools.com/api/people/18/';
