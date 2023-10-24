bfb0a3a70cc9c0688ba056f93a9c16f7bbb0f6d4
saaid323 <saaid2538@gmail.com>
2023-10-24 04:07:18 -0700

    added 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,5 +2,9 @@
 const fs = require('fs');
 const path = process.argv[2];
 fs.readFile(path, 'utf8', (err, data) => {
-	console.log(data);
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+  console.log(data);
 });
